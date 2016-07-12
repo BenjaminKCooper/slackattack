f3e8f0e1fa85c1989f0375832c10f50944dc2001
Benjamin Cooper <benjamin.k.cooper.18@dartmouth.edu>
2016-07-12 02:13:50 -0400

    updated

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 import botkit from 'botkit';
 require('dotenv').config(); // I struggled to get my environmental variables working correctly, so I went with the dotenv method of importing instead.
 
+
 // I learned about the Yelp API from the following link: https://github.com/olalonde/node-yelp .
 const Yelp = require('yelp');
 
@@ -11,23 +12,16 @@ const yelp = new Yelp({
   token_secret: process.env.YELP_TOKEN_SECRET,
 });
 
-const yelpSearch = (response, convo) => {
-  let foodSearchFINAL = convo.extractResponse('foodTypeYelp');
-  let citySearchFINAL = convo.extractResponse('yelpLocationResponse');
-  yelp.search({ term: foodSearchFINAL, location: citySearchFINAL })
-  .then((data) => {
-    convo.say(data.businesses[0].name); // console.log(data);
-  })
-  .catch((err) => {
-    convo.say('Hmm... could not really find anything...');
-  });
-};
 
 // example bot
 
 // botkit controller
 const controller = botkit.slackbot({
   debug: false,
+});
+//
+controller.on('outgoing_webhook', (bot, message) => {
+  bot.replyPublic(message, 'yeah yeah');
 });
 
 // initialize slackbot
@@ -40,26 +34,22 @@ const slackbot = controller.spawn({
   if (err) { throw new Error(err); }
 });
 
-const askWhatFoodType = (response, convo) => {
-  const foodTypeResp = { key: 'foodTypeYelp', multiple: false };
-  convo.ask('Yea, I am hungry too!! What type of food tickles your fancy today?', () => {
-    convo.say('Awesome.');
-    askLocation(response, convo);
-    convo.next();
-  }, foodTypeResp);
+
+const smallTalkPart2 = (response, convo) => {
+  const resp = convo.extractResponse('smallTalkResp1');
+  if (resp === 'good') {
+    convo.say('Yay!! I am so glad to hear.');
+  } else {
+    convo.say('Oh no!! I am sorry to hear.');
+  }
 };
 
-const askLocation = (response, convo) => {
-  const yelpLocationResponse = { key: 'yelpLocationResponse', multiple: false };
-  // let foodType = convo.extractResponse('foodTypeYelp');
-  // convo.say(foodType);
-  convo.say(convo.extractResponse('foodTypeYelp'));
-
-  convo.ask('In what town and state are you looking to eat?', () => {
-    yelpSearch(response, convo);
+const smallTalkPart1 = (response, convo) => {
+  const howAreYouResp = { key: 'smallTalkResp1', multiple: false };
+  convo.ask('I am doing well, thank you for asking! How are you doing?', () => {
+    smallTalkPart2(response, convo);
     convo.next();
-  }, yelpLocationResponse);
-  // yelpSearch(response, convo);
+  }, howAreYouResp);
 };
 
 
@@ -74,9 +64,76 @@ controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 
   });
 });
 
-controller.hears(['food', 'hungry', 'eat'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-  bot.startConversation(message, askWhatFoodType); // I learned about conversations from the example file "/node_modules/botkit/convo_bot.js".
+controller.hears(['sup', 'how are you', 'whats up'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.startConversation(message, smallTalkPart1);
 });
+
+controller.hears(['what are you even talking about'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.reply(message,
+    {
+      // text: 'Lots of good options out there, but this seems to be the best bet',
+      attachments: [
+        {
+          title: 'Oh no!! Looks like we are having a bit of a misscommunication...',
+          image_url: 'http://img.ifcdn.com/images/b4b49a08ad0d7129ff1f107b2355d5a0ea23f65b66add4cfc6ad3a39df335724_1.jpg',
+        },
+      ],
+    });
+});
+
+controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.reply(message, 'Here are a few things we can talk about: Your next meal (just some derivation of food/hungry), how your day is going.');
+});
+
+
+controller.hears(['food', 'hungry', 'eat'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.startConversation(message, (resp, convo) => {
+    const foodTypeResp = { key: 'foodTypeYelp', multiple: false };
+    convo.ask('Yea, I am hungry too!! What type of food tickles your fancy today?', (response) => {
+      // askLocation(response, convo);
+
+      // (response, convo) => {
+      const yelpLocationResponse = { key: 'yelpLocationResponse', multiple: false };
+        // let foodType = convo.extractResponse('foodTypeYelp');
+        // convo.say(foodType);
+      convo.say(convo.extractResponse('foodTypeYelp'));
+      convo.next();
+
+      convo.ask('In what town and state are you looking to eat?',
+          // yelpSearch(response, convo);
+          () => {
+            const foodSearchFINAL = convo.extractResponse('foodTypeYelp');
+            const citySearchFINAL = convo.extractResponse('yelpLocationResponse');
+
+            yelp.search({ term: foodSearchFINAL, location: citySearchFINAL, limit: 1 })
+            .then((data) => {
+              bot.reply(message,
+                {
+                  text: 'Lots of good options out there, but this seems to be the best bet',
+                  attachments: [
+                    {
+                      title: `${data.businesses[0].name}`,
+                      title_link: `${data.businesses[0].url}`,
+                      text: `${data.businesses[0].snippet_text}`,
+                      image_url: `${data.businesses[0].image_url}`,
+                    },
+                  ],
+                });
+              // convo.say(data.businesses[0].name); // console.log(data);
+            })
+            .catch((err) => {
+              convo.say('Hmm... could not really find anything...');
+            });
+            convo.next();
+
+
+            // convo.next();
+          }, yelpLocationResponse);
+      convo.next();
+    }, foodTypeResp);
+    convo.next();
+  });
+}); // I learned about conversations from the example file "/node_modules/botkit/convo_bot.js".
 
 // prepare webhook
 // for now we won't use this but feel free to look up slack webhooks
